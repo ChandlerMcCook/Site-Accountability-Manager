e0c4b33f204acdc5e8a465014043b7243166ad75
ChandlerMcCook <chandler.mccook@gmail.com>
2025-02-09 20:42:45 -0600

    need to fix chartPopup

--- src/chartPopup.js
+++ src/chartPopup.js
@@ -3,14 +3,20 @@ import { Chart, registerables } from "chart.js"
 Chart.register(...registerables)
 
 document.addEventListener("DOMContentLoaded", () => {
-    let timeData = {
-        "www.facebook.com": 200000,
-        "www.google.com": 2866744,
-        "www.linkedin.com": 1991200,
-        "www.reddit.com": 757504,
-        "www.twitter.com": 100000,
-        "www.youtube.com": 9323897
-    }
+    // let timeData = {
+    //     "www.facebook.com": 200000,
+    //     "www.google.com": 2866744,
+    //     "www.linkedin.com": 1991200,
+    //     "www.reddit.com": 757504,
+    //     "www.twitter.com": 100000,
+    //     "www.youtube.com": 9323897
+    // }
+
+    let timeData = chrome.storage.local.get("trackedSites")
+    console.log(JSON.stringify(timeData))
+    timeData = timeData.trackedSites || {}
+
+    console.log(JSON.stringify(timeData))
 
     const siteNames = Object.keys(timeData)
         .map(name => name.slice(4, -4))
