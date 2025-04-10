9267f00ba8a0f1105295a8a64b087c347d761c66
ChandlerMcCook <chandler.mccook@gmail.com>
2025-04-10 18:02:03 -0500

    taken away whitelist

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,13 +2,11 @@ import { getLocalData } from "../../helper-functions/getLocalData"
 import { navbarLogic } from "./settings-helpers/navbar"
 import { modalLogic } from "./settings-helpers/modal"
 import { themeLogic } from "./settings-helpers/theme"
-import { whitelistLogic } from "./settings-helpers/whitelist"
 
 document.addEventListener("DOMContentLoaded", async () => {
     navbarLogic()
     modalLogic()
     themeLogic()
-    whitelistLogic()
     
     // accountability checkbox logic
     const curAccountability = await getLocalData("accountability")
